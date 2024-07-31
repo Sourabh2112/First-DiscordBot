16a7d4a137c2c278c96eb0bb2d874950c4975d12
sourabh kumar <sourabhsbg01@gmail.com>
2024-07-31 13:13:52 +0530

    option and choice

---
+++ src/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`✅ ${c.user.tag} is online.`);
+});
+
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'hey') {
+        return interaction.reply('hey!');
+    }
+
+    if (interaction.commandName === 'ping') {
+        return interaction.reply('Pong!');
+    }
+
+    if (interaction.commandName === 'add') {
+        const num1 = interaction.options.get('first-number').value;
+        const num2 = interaction.options.get('second-number').value;
+
+        interaction.reply(`The sum is ${num1 + num2}`);
+    }
+
+    
+});
+
+client.login(process.env.TOKEN);
